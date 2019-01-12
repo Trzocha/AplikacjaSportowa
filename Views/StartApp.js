b7ee2801d8e7247f4c696080dbc6526f7b624045
Trzocha <sebastiantrzoska@gmail.com>
2019-01-12 17:50:35 +0100

    Delete StartApp.js

--- Views/StartApp.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ControlView from "../Views/ControlView";
-
-const StartApp = props => {
-  return <>{props.active ? <ControlView data={props.data} /> : null}</>;
-};
-
-export default StartApp;
