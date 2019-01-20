2f57bdd96ff9c58c9ae5696e66738ceb4e056575
Trzocha <sebastiantrzoska@gmail.com>
2019-01-20 22:17:28 +0100

    Add files via upload

---
+++ precache-manifest.60c7237a7706c50e96ef93cec6f4f109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "30e7188553c7f0f184af",
+    "url": "/static/js/main.30e71885.chunk.js"
+  },
+  {
+    "revision": "163a68a7469baa40ca7b",
+    "url": "/static/js/1.163a68a7.chunk.js"
+  },
+  {
+    "revision": "30e7188553c7f0f184af",
+    "url": "/static/css/main.d35ee627.chunk.css"
+  },
+  {
+    "revision": "4011e510db82ab788fcd21848c94cda6",
+    "url": "/index.html"
+  }
+];
